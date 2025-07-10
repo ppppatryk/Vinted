6a48816852aa9e20f5572445cd2a2699b8557157
ppppatryk <patrykhan@onet.pl>
2025-07-10 19:07:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "📥 Pobieram Chromium..."
-#nowy
-https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chrome-linux64.zip
-unzip -qq chrome-linux64.zip
 
-echo "✅ Nadaję prawa do uruchamiania..."
+echo "📥 Pobieram Chromium..."
+wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chrome-linux64.zip
+unzip chrome-linux64.zip
 chmod +x chrome-linux64/chrome
+
 
 echo "✅ Gotowe. Uruchamiam bota..."
 python bot.py
